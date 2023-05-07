1c970f16ea82f08c8784e4309618a28bebb7eb82
naruhikohama <naruhikohama@gmail.com>
2023-05-07 15:55:51 -0300

    Connecting to API to extract data

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,8 +1,11 @@
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
 
+import json
 from datetime import datetime
+
 with DAG('user_processing', start_date = datetime(2023, 4, 1),
          schedule_interval='@daily', catchup = False) as dag:
     
@@ -25,4 +28,13 @@ with DAG('user_processing', start_date = datetime(2023, 4, 1),
         task_id = 'is_api_available',
         http_conn_id = 'user_api',
         endpoint = 'api/'
+    )
+
+    extract_user = SimpleHttpOperator(
+        task_id = 'extract_user',
+        http_conn_id = 'user_api',
+        endpoint = 'api/',
+        method = 'GET', 
+        response_filter = lambda response: json.loads(response.text),
+        log_response = False
     )
