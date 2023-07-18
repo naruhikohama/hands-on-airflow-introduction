c6b06680a22cc86f4a957d5fa572889037ff48a5
naruhikohama <naruhikohama@gmail.com>
2023-07-18 08:30:17 -0300

    trigger rules

--- dags/xcom_dag.py
+++ dags/xcom_dag.py
@@ -39,4 +39,10 @@ with DAG("xcom_dag", start_date=datetime(2023, 7, 1),
         bash_command="echo ''"
     )
  
-    t1 >> branch >> [t2, t3]
+    t4 = BashOperator(
+        task_id='t4',
+        bash_command="echo ''",
+        trigger_rule='none_failed_min_one_success'
+    )
+ 
+    t1 >> branch >> [t2, t3] >> t4
