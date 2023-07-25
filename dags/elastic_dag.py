f52fc399a65a54fcb94e84a7141445fa7a0789d5
naruhikohama <naruhikohama@gmail.com>
2023-07-24 23:18:54 -0300

    creating dag that uses elastic hook plugin

---
+++ dags/elastic_dag.py
@@ -0,0 +1,19 @@
+from airflow import DAG 
+from airflow.operators.python import PythonOperator
+from hooks.elastic.elastic_hook import ElasticHook
+
+from datetime import datetime
+
+def _print_es_info():
+    hook = ElasticHook()
+    print(hook.info()) 
+
+with DAG('elastic_dag', 
+         start_date=datetime(2023, 1, 1),
+         schedule_interval='@daily',
+         catchup=False):
+    
+    _print_es_info = PythonOperator(
+        task_id = 'print_es_info',
+        python_callable=_print_es_info
+    )
